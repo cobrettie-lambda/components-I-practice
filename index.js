68bfc2563c7b612bef801548f3dfa5da38f3d619
Cobrettie <cobrettiegarner3@gmail.com>
2019-05-06 19:48:12 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// Steps to creating components
+
+// STEP 1.) Select some DOM elements that we might use a lot
+
+const originalPanels = document.querySelectorAll('.panel');
+// console.log(originalPanels);
+
+
+// STEP 2.) Return newly constructed DOM elements
+originalPanels.forEach(function (panel) {
+    // console.log(panel);
+});
