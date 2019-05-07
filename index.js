447274ee566596b3008fabe497fefd5d8d5f46da
Cobrettie <cobrettiegarner3@gmail.com>
2019-05-06 20:40:29 -0400

    Add explicit binding example, fat arrow example

--- index.js
+++ index.js
@@ -5,15 +5,27 @@ class Panel {
     constructor(panel) {
         this.panel = panel;
         this.panelButtons = this.panel.querySelector('.panel-buttons')
-        this.panelButtonOpen = this.panel.querySelector('.panel-btn-open');
-        this.panelButtonClose = this.panel.querySelector('.panel-btn-close');
+        this.panelBtnOpen = this.panel.querySelector('.panel-btn-open');
+        this.panelBtnClose = this.panel.querySelector('.panel-btn-close');
         this.panelContent = this.panel.querySelector('.panel-content');
-        this.panelButtons.addEventListener('click', this.togglePanel());
+
+        // EXPLICIT BINDING with .bind
+        this.panelButtons.addEventListener('click', this.togglePanel.bind(this));
+
+        // FAT ARROWS
+        this.panelButtons.addEventListener('click', () => this.togglePanel());
 
     }
     // different scope block
     togglePanel() {
-        console.log('I was clicked');
+        // When only one argument is present: Toggle the class value, i.e., if the class exists then remove it and return false, if not, then add it and return true
+
+        // console.log('I was clicked');
+        this.panelBtnOpen.classList.toggle('hide-btn');
+        this.panelBtnClose.classList.toggle('hide-btn');
+        this.panelContent.classList.toggle('toggle-on');
+
+
     }
 }
 
